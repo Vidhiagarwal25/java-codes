1442d9317dd1f0888e79893d6984733f5bc9e171
VIDHI AGARWAL <vidhi21btai44@karnavatiuniversity.edu.in>
2024-06-08 16:33:11 +0530

    codes

---
+++ finalvar.java
@@ -0,0 +1,16 @@
+public class  finalvar{
+    public static void main(String[] args) {
+        // normal variable
+        int a =  2;
+        System.out.println(a);
+        a = 3;
+        System.out.println(a);
+        // final variable
+        final int b = 6;
+        System.out.println(b);
+        System.out.println("can it change?");
+        b = 3;
+        System.out.println(b);
+        System.out.println("no final variable changes");
+    }
+}
